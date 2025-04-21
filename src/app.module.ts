d2d072eb921d9e52d2ec5bb7e5d5a843314aa8bb
Luka <lussskki@gmail.com>
2025-04-21 17:53:58 +0400

    every feature comes here implemented in here

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cofigService: ConfigService) => ({
+        uri:cofigService.get<string>('DB_URI')
+      })
+    }),
+    BookModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
