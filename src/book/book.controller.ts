26487f062d7cbe4a016e00735e66924b8b76c91f
Luka <lussskki@gmail.com>
2025-04-22 19:02:32 +0400

    add CreateBookDto

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,22 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { BookService } from './book.service';
-import { Book } from './schemas/book.schema';
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { BookService } from './book.service'
+import { Book } from './schemas/book.schema'
+import { CreateBookDto } from './dto/create-book.dto' 
 
 @Controller('books')
 export class BookController {
-    constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService) {}
 
-    @Get()
-    async getAllBooks(): Promise<Book[]> {
-        return this.bookService.findAll()
-    }
-    
-    @Post() 
-    async createBook(
-        @Body()
-        book
-    ): Promise<Book> {
-        return this.bookService.create(book)
-    }
-    
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll()
+  }
+
+  @Post()
+  async createBook(@Body() book: CreateBookDto): Promise<Book> {
+    return this.bookService.create(book)
+  }
 }
